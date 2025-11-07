fe8c4a91b02923d835d0a09f5ff0f77ea66835d8
mahakwalia102@gmail.com <mahakwalia102@gmail.com>
2025-11-07 14:48:07 +0000

    test

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,10 +1,26 @@
 {{ config(materialized='table') }}
 
+{% set rating_categories = ["ss_net_paid",
+                            "ss_net_paid_inc_tax",
+                            "ss_net_profit"] %}
+
 with source_data as (
 
-    select * from {{ source('store_data', 'store') }}
-
+    
+    SELECT ss_item_sk,ss_sold_date_sk,
+    {%- for col_name in rating_categories -%}
+    AVG({{ col_name }}) as {{ column_name }}_average
+    {%- if not loop.last  -%} 
+        , 
+    {%- endif -%}
+    {%- endfor -%}
+    
+    FROM {{ source('store_data', 'store_sales') }}  
+    GROUP BY 1,2
 )
 
-select *
-from source_data
+select ss.*, i.i_category, i.i_product_name
+from source_data ss 
+left join {{ source('store_data', 'item') }} i 
+    on ss.ss_item_sk = i.i_item_sk
+
